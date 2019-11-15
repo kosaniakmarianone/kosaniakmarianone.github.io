f9ba82f9c9c1ddb3cb89dc0dcec2182b9a94437c
Marik1 <kosaniakm@gmail.com>
2019-11-16 01:03:56 +0200

    ve

---
+++ temes.js
@@ -0,0 +1,84 @@
+var temes = [
+    {
+        tema: 'Вступний урок',
+        task: '', 
+        materials: '',
+        video: 'https://www.youtube.com/watch?v=XWIbP87SrWA&feature=youtu.be'
+    },
+    {
+        tema: 'Заголовки, введення в CSS', 
+        task: '',
+        materials: '', 
+        video: 'https://www.youtube.com/watch?v=8KSKvlqDYbw&feature=youtu.be'
+    },
+    {
+        tema: 'Форматування тексту і фон сторінки', 
+        task: [
+            't3/z1/index.html',
+            't3/z2/index.html',
+            't3/z3/index.html'
+        ], 
+        materials: 'https://drive.google.com/drive/folders/1eMiTjEbRO5YeOhr_1UW-jiGMa4hnbbZZ', 
+        video: 'https://www.youtube.com/watch?v=Fe1zISWD-uE&feature=youtu.be'
+    },
+    {
+        tema: 'Списки, тег hr', 
+        task: [
+            't4/1.1.html',
+            't4/2.2.html',
+            't4/3_1.html'
+        ], 
+        materials: 'https://drive.google.com/drive/folders/1QHr-A_YWnPYpQ7ZAJRN6x7n4AXlQyGdS', 
+        video: 'https://www.youtube.com/watch?v=eIXApww4qx0&feature=youtu.be'
+    },
+    {
+        tema: 'Самостійна робота', 
+        task: '',
+        materials: '', 
+        video: ''
+    },
+    {
+        tema: 'Робота над помилками, розвиваючі вправи', 
+        task: '', 
+        materials: '', 
+        video: ''
+    },
+    {
+        tema: 'Робота з Git #1', 
+        task: '', materials: '', 
+        video: 'https://www.youtube.com/watch?v=J5_4G909T14&feature=youtu.be'},
+    {
+        tema: 'Робота з Git #2', 
+        task: '', 
+        materials: '', 
+        video: ''
+    },
+    {
+        tema: 'Робота з Git #3', 
+        task: '',
+        materials: '', 
+        video: ''
+    },
+    {
+        tema: 'Робота з Git #4', 
+        task: '', 
+        materials: '', 
+        video: ''
+    },
+    {
+        tema: 'Робота з Git #5', 
+        task: '',
+        materials: '', 
+        video: ''
+    },
+    {
+        tema: 'Робота з зображеннями в HTML, CSS', 
+        task: [
+            't12/1/img_1.html',
+            't12/2/img_2.html',
+            't12/3/img_3.html'
+        ], 
+        materials: 'https://drive.google.com/drive/folders/1d7DQ8NrSjL7oOGeVhM7GII3InORer-qN', 
+        video: 'https://www.youtube.com/watch?v=gx8uKZZna2g&feature=youtu.be'
+    }
+]
