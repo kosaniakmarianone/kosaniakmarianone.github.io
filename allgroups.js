02d012e7a10b5c5a382b29e37edd74c16faffa62
Marik1 <kosaniakm@gmail.com>
2019-11-17 21:52:47 +0200

    add shit login

---
+++ allgroups.js
@@ -0,0 +1,14 @@
+var allgroups = [
+    {
+        school: 'Надія',
+        groups: [
+            {name: 'SL-102', password: 'SL102kmv', temes_pass: 3},
+            {name: 'SL-103', password: 'SL103kmv', temes_pass: 7},
+            {name: 'SL-104', password: 'SL104kmv', temes_pass: 7},
+            {name: 'SL-102s', password: 'SL102kmv', temes_pass: 7},
+            {name: 'SL-103s', password: 'SL103kmv', temes_pass: 7},
+            {name: 'SL-104s', password: 'SL104kmv', temes_pass: 7}
+        ]
+    }
+    //тут добавляти інші заклади і відповідно групи в них
+]
