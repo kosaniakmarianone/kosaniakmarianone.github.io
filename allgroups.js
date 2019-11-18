47faeb57f7ed7f7bd1c3996c69791260f5237764
Marik1 <kosaniakm@gmail.com>
2019-11-18 14:57:23 +0200

    fix login

--- allgroups.js
+++ allgroups.js
@@ -9,6 +9,17 @@ var allgroups = [
             {name: 'SL-103s', password: 'SL103kmv', temes_pass: 7},
             {name: 'SL-104s', password: 'SL104kmv', temes_pass: 7}
         ]
+    },
+    {
+        school: 'Надія2',
+        groups: [
+            {name: 'SL-102', password: 'SL102kmv', temes_pass: 5},
+            {name: 'SL-103', password: 'SL103kmv', temes_pass: 1},
+            {name: 'SL-104', password: 'SL104kmv', temes_pass: 3},
+            {name: 'SL-102s', password: 'SL102kmv', temes_pass: 5},
+            {name: 'SL-103s', password: 'SL103kmv', temes_pass: 4},
+            {name: 'SL-104s', password: 'SL104kmv', temes_pass: 4}
+        ]
     }
     //тут добавляти інші заклади і відповідно групи в них
 ]
