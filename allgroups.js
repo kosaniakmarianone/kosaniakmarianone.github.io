5e8c8eb41b1a1a892259cae501153aff9672addc
Marik1 <kosaniakm@gmail.com>
2019-11-22 12:38:55 +0200

    add register

--- allgroups.js
+++ allgroups.js
@@ -2,12 +2,12 @@ var allgroups = [
     {
         school: 'Надія',
         groups: [
-            {name: 'SL-102', password: '102kmv', temes_pass: 7},
-            {name: 'SL-103', password: '103kmv', temes_pass: 7},
-            {name: 'SL-104', password: '104kmv', temes_pass: 7},
-            {name: 'SL-102s', password: '102kmv', temes_pass: 7},
-            {name: 'SL-103s', password: '103kmv', temes_pass: 7},
-            {name: 'SL-104s', password: '104kmv', temes_pass: 7}
+            {name: 'SL-102', password: '102kmv', temes_pass: 8},
+            {name: 'SL-103', password: '103kmv', temes_pass: 8},
+            {name: 'SL-104', password: '104kmv', temes_pass: 8},
+            {name: 'SL-102s', password: '102kmv', temes_pass: 8},
+            {name: 'SL-103s', password: '103kmv', temes_pass: 8},
+            {name: 'SL-104s', password: '104kmv', temes_pass: 8}
         ]
     },
     {
