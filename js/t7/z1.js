c170eb556e854a760d36e6989e3b590e191bfe85
MarikKV <39519934+MarikKV@users.noreply.github.com>
2020-11-03 21:48:35 +0200

    Add files via upload
    add t6-t7

---
+++ js/t7/z1.js
@@ -0,0 +1,17 @@
+
+let arr_1 = [];
+/*
+while(arr_1.length < 4){
+	const item = prompt("enter number")
+	
+	arr_1.push(item);
+}	
+
+console.log(arr_1)
+*/
+
+for( i = 1; arr_1.length < 4; i++){
+	const item = prompt("enter number")
+	arr_1.push(item);
+}
+console.log(arr_1)
