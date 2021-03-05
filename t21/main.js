65cb6067185877d6ff995831610344f17799e85b
Marik <kosaniakm@gmail.com>
2021-03-05 19:48:15 +0200

    add t21

---
+++ t21/main.js
@@ -0,0 +1,22 @@
+const test = document.getElementById('test');
+
+test.setAttribute('align', 'right');
+
+test.setAttribute('style', 'color: green; background: red');
+
+function changeCard() {
+    const card_num   = document.getElementById('card_num').value;
+    const card_color = document.getElementById('card_color').value;
+    const card_bg    = document.getElementById('card_bg').value;
+
+    const id = `card_${card_num}`;
+
+    document.getElementById(id).setAttribute('style', 
+    
+    `color: ${card_color}; background: ${card_bg}`)
+
+    console.log(card_num, card_color, card_bg)
+} 
+
+git config --global user.email "you@example.com"
+git config --global user.name "Your Name"
