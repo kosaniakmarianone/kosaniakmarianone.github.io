cdee4fa0858728ac809e9d2a2500ad3655a38dd6
MarikKV <kosaniakm@gmail.com>
2022-03-04 20:02:46 +0200

    test auth

---
+++ js_2/t15/done/main.js
@@ -0,0 +1,75 @@
+document.addEventListener('DOMContentLoaded', async function(){   
+
+    let home =     await axios.get("templates/home.html");
+    let products = await axios.get("templates/products.html");
+    let orders =   await axios.get("templates/orders.html");
+    let notfound = await axios.get("templates/404.html");
+
+    const data =  {
+        message: 'Hello Vue.js!',
+        currentPath: window.location.hash,
+    };
+
+    const Home = {
+        template: home.data,
+    };
+
+    const NotFound = {
+        template: notfound.data
+    };
+
+    const Products = {
+        template: products.data
+    };
+
+    const Orders = {
+        template: orders.data
+    };
+
+    const routes = {
+        '/': Home,
+        '/home': Home,
+        '/products': Products,
+        '/orders': Orders,
+        '/not-found': NotFound,
+    }
+
+    const app = {
+        data() {
+            return data
+        },
+        methods: {
+            test() {
+                this.message = 3
+            },
+            googleAuth(){
+                console.log("auth started...")
+                firebase.auth()
+                .signInWithPopup(provider)
+                .then( result => {
+                    const user = result.user;
+                    console.log(result)
+                    console.log(user)
+                })
+                .catch((error) => {
+                    console.log(error)
+                });
+            } 
+            
+        },
+        components: {
+            
+        },
+        computed: {
+            currentView() {
+                return routes[this.currentPath.slice(1) || '/'] || NotFound
+            }
+        },
+        mounted() {
+            window.addEventListener('hashchange', () => {
+                this.currentPath = window.location.hash
+            })
+        }
+    }
+    Vue.createApp(app).mount('#app');
+})
