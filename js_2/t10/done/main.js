9706e751a253dbc58118ee762ea41dad4ec99727
DESKTOP-E5Q8V3G\Marian <marian@tarta.ai>
2023-03-18 14:04:48 +0200

    update t10

--- js_2/t10/done/main.js
+++ js_2/t10/done/main.js
@@ -1,26 +1,45 @@
-const test =  {
+const data =  {
   message: 'Hello Vue.js!',
-  kek: 2,
-  lol: 10,
-  products: ["one", "two"]
+  social_1: { 
+    img: "https://castironsteak.com/wp-content/uploads/2016/01/google-square.jpg",
+    name: "Google"
+  },
+  social_2: { 
+    img: "https://cdn2.downdetector.com/static/uploads/logo/FB-f-Logo__blue_512.png",
+    name: "Facebook"
+  },
+  socials: [
+    { 
+      img: "https://castironsteak.com/wp-content/uploads/2016/01/google-square.jpg",
+      name: "Google"
+    },
+    { 
+      img: "https://cdn2.downdetector.com/static/uploads/logo/FB-f-Logo__blue_512.png",
+      name: "Facebook"
+    }
+  ],
 };
 
-const CardProduct = {
-  props: ['todo'],
-  template: `<li>{{ todo }}</li>`
+const SocialsComponent = {
+  props: ['social'],
+  template: `
+  <div class="card" style="width: 200px; padding: 10px; margin: 10px;">
+    <h2 class="text-center">{{social.name}}</h2>
+    <img v-bind:src="social.img" style='width:100px; height: 100px; margin:0 auto;'>
+  </div>`
 };
+
+
 
 const app = {
   data() {
-    return test
+    return data
   },
   methods: {
-    test() {
-      this.message = 3
-    } 
+   
   },
   components: {
-    CardProduct
+    SocialsComponent
   }
 }
 
