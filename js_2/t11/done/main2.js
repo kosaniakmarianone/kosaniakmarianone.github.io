6169dd4df989a434c56fa493f97c2acce9189096
DESKTOP-E5Q8V3G\Marian <marian@tarta.ai>
2023-04-01 11:37:25 +0300

    add modal

--- js_2/t11/done/main2.js
+++ js_2/t11/done/main2.js
@@ -1,15 +1,10 @@
 const data =  {
   message: 'Hello Vue.js!',
-  comments: [
-    { 
-      userName: "Rainer Brous",
-      text: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Minima assumenda maiores odio dolorem, ratione iste possimus inventore esse soluta, autem obcaecati est impedit vel minus sit praesentium? Odit, nihil tempora."
-    },
-    { 
-      userName: "Annie Leonhart",
-      text: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Minima assumenda maiores odio dolorem, ratione iste possimus inventore esse soluta, autem obcaecati est impedit vel minus sit praesentium? Odit, nihil tempora."
-    }
-  ]
+  comments: [],
+  newComment: {
+    userName: "Test user",
+    text: "Text text"
+  }
 };
 
 const CommentCommponent = {
@@ -37,23 +32,37 @@ const app = {
   data() {
     return data
   },
+
   methods: {
    getCommentsFormDB(){
-    db.collection("comments").get().then( res => {
-      this.comments = [];  //очишуємо коментарі при їх новому завантаженні
-      res.forEach((doc) => {
-            const comment = doc.data();
-            comment.id = doc.id;
-            this.comments.push(comment);
-            console.log(comment)
-        });
-      console.log(this.comments)
-    });
-   }
+      db.collection("comments").get().then( res => {
+        this.comments = [];  //очишуємо коментарі при їх новому завантаженні
+        res.forEach((doc) => {
+              const comment = doc.data();
+              comment.id = doc.id;
+              this.comments.push(comment);
+              console.log(comment)
+          });
+      });
+   },
+
+   addNewComment(){
+      if(this.newComment.userName != "" && this.newComment.text != ""){
+        db.collection("comments").add(this.newComment)
+        .then( res => {
+          console.log('new comment added!');
+          this.newComment.userName = "";
+          this.newComment.text = "";
+          this.getCommentsFormDB();
+        })
+      }
+   },
   },
+
   components: {
     CommentCommponent
   },
+
   mounted() {
     // Витягуємо коментарі з бази данних при 1му завантаженні
     this.getCommentsFormDB()
