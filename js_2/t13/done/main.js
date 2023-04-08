f85b16cbf2af7e18d58ad3061ba0d7e82fbd222b
DESKTOP-E5Q8V3G\Marian <marian@tarta.ai>
2023-04-08 12:07:15 +0300

    fix theme 13

--- js_2/t13/done/main.js
+++ js_2/t13/done/main.js
@@ -1,1 +1,48 @@
-console.log('')
+document.addEventListener('DOMContentLoaded', async function(){
+    let card_template = await axios.get("card-template.html");
+    
+    console.log(card_template)
+    console.log(card_template.data)
+
+    const data =  {
+        message: 'Hello Vue.js!',
+        products: []
+    };
+
+    const CardProduct = {
+        props: ['product'],
+        template: card_template.data,
+        methods: {
+            testClick(){
+                console.log(this.product);
+            }
+        }
+    };
+
+    const app = {
+        data() {
+            return data
+        },
+        methods: {
+            getProducts(){
+                db.collection("products").get().then( res => {
+                    this.products = [];  //очишуємо коментарі при їх новому завантаженні
+                    res.forEach((doc) => {
+                          const product = doc.data();
+                          product.id = doc.id;
+                          this.products.push(product);
+                          console.log(product)
+                      });
+                  });
+            }
+        },
+        components: {
+            'card': CardProduct
+        },
+        mounted() {
+            // Витягуємо товари з бази данних при 1-му завантаженні
+            this.getProducts()
+          }
+    }
+    Vue.createApp(app).mount('#app');
+})
